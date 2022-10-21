84ddf94799d32ff8e80bdff59d65871e48c256c7
Alvin Martin Djong <75116369+alvinmdj@users.noreply.github.com>
2022-10-21 17:13:35 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://go-crowdfunding.herokuapp.com',
+    baseURL: 'https://go-crowdfunding.onrender.com',
   },
 
   // Auth with local schemes: https://auth.nuxtjs.org/schemes/local
